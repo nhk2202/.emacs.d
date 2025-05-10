0d8d2e20e669c02f58b4c256863282fe99fea55e
Hải Khánh <haikhanh220204@gmail.com>
2025-05-10 15:11:16 +0700

    Miscellaneous updates

--- init.el
+++ init.el
@@ -20,13 +20,16 @@
 (load custom-file)
 
 (setq isearch-repeat-on-direction-change t
-      isearch-wrap-pause 'no)
+      isearch-wrap-pause 'no
+      isearch-allow-scroll t
+      query-replace-skip-read-only t)
 
 (setq tab-bar-show 1)
 (column-number-mode 1)
 (setq-default fill-column 80)
 (setq truncate-lines t)
 (setq scroll-margin 10)
+(global-hl-line-mode 1)
 
 (repeat-mode 1)
 
@@ -34,7 +37,6 @@
 (which-key-mode 1)
 (diminish 'which-key-mode)
 
-(setq read-answer-short t)
 (setq enable-recursive-minibuffer t
       read-extended-command-predicate #'command-completion-default-include-p
       minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt)
@@ -53,30 +55,35 @@
 (setq mark-even-if-inactive nil
       delete-selection-mode t)
 
+(setq kill-do-not-save-duplicates t)
+
 (require-theme 'modus-themes)
 (setq modus-themes-bold-constructs t
       modus-themes-italic-constructs t
       modus-themes-prompts '(heavy)
       modus-themes-common-palette-overrides '((comment yellow-cooler)
-					      (string green-cooler)
-					      (bg-mode-line-active bg-sage)
-					      (fg-mode-line-active fg-main)
-					      (border-mode-line-active bg-green-intense)
-					      (fg-prompt cyan)
-					      (bg-prompt bg-cyan-nuanced))
-      modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
+                                              (string green-cooler)
+                                              (bg-mode-line-active bg-sage)
+                                              (fg-mode-line-active fg-main)
+                                              (border-mode-line-active bg-green-intense)
+                                              (fg-prompt cyan)
+                                              (bg-prompt bg-cyan-nuanced))
+      modus-themes-to-toggle '(modus-operandi modus-vivendi))
 (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
-(modus-themes-load-theme 'modus-vivendi-tinted)
+(modus-themes-load-theme 'modus-vivendi)
 
 (use-package magit
   :bind ("C-c g" . magit))
+
+(use-package forge
+  :after magit)
 
 (use-package vertico
   :custom
   (vertico-resize t)
   :config
   (setq completion-styles '(basic partial-completion flex)
-	completion-ignore-case t)
+        completion-ignore-case t)
   (keymap-set vertico-map "C-S-n" #'vertico-next-group)
   (keymap-set vertico-map "C-S-p" #'vertico-previous-group)
   (vertico-mode 1))
@@ -85,8 +92,8 @@
   :after vertico
   :ensure nil
   :bind (:map vertico-map
-	      ("RET" . vertico-directory-enter)
-	      ("DEL" . vertico-directory-delete-char))
+         ("RET" . vertico-directory-enter)
+         ("DEL" . vertico-directory-delete-char))
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
 (use-package vertico-multiform
@@ -99,27 +106,30 @@
 
 (use-package marginalia
   :bind (:map minibuffer-local-map
-	      ("M-a" . marginalia-cycle))
+         ("M-a" . marginalia-cycle))
   :init
   (marginalia-mode 1))
 
 (use-package consult
-  :bind (("C-c k" . consult-kmacro)
-	 ("C-h C-i" . consult-info)
-	 ([remap Info-search] . consult-info)
-	 ("C-y" . consult-yank-from-kill-ring)
-	 ("C-x b" . consult-buffer)
-	 ("C-x 4 b" . consult-buffer-other-window)
-	 ("C-x t b" . consult-buffer-other-tab)
-	 ("C-c c e" . consult-compile-error)
-	 ("C-c c f" . consult-flymake)
-	 ("C-c s f" . consult-fd)
-	 ("C-c s g" . consult-ripgrep)
-	 ("C-c s G" . consult-git-grep)
-	 ("C-x <ESC> <ESC>" . consult-complex-command)
-	 :map minibuffer-local-map
-	 ([remap next-matching-history-element] . consult-history)
-	 ([remap previous-matching-history-element] . consult-history))
+  :bind (("C-x e" . consult-kmacro)
+         ("C-h C-i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ("C-y" . consult-yank-from-kill-ring)
+         ("C-x b" . consult-buffer)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x t b" . consult-buffer-other-tab)
+         ("C-x r <RET>" . consult-register)
+         ("C-c c e" . consult-compile-error)
+         ("C-c c f" . consult-flymake)
+         ("M-g g" . consult-line)
+         ("M-g M-g" . consult-line-multi)
+         ("C-c s f" . consult-fd)
+         ("C-c s g" . consult-ripgrep)
+         ("C-c s G" . consult-git-grep)
+         ("C-x <ESC> <ESC>" . consult-complex-command)
+         :map minibuffer-local-map
+         ([remap next-matching-history-element] . consult-history)
+         ([remap previous-matching-history-element] . consult-history))
   :custom
   (consult-narrow-key "M--")
   (consult-widen-key "M-=")
@@ -127,7 +137,7 @@
   (advice-add #'register-preview :override #'consult-register-window)
   (setq register-preview-delay 0.5)
   (setq xref-show-xrefs-function #'consult-xref
-	xref-show-definition-function #'consult-xref)
+        xref-show-definition-function #'consult-xref)
   :config
   (consult-customize
    consult--source-bookmark
@@ -138,17 +148,15 @@
 
 (use-package embark
   :bind (("C-." . embark-act)
-	 ("M-." . embark-dwim)
-	 ("C-h b" . embark-bindings))
+         ("C-;" . embark-dwim))
   :custom
-  (embark-help-key "?")
   (embark-quit-after-action nil)
   :init
   (setq y-or-n-p-use-read-key t)
   (setq prefix-help-command #'embark-prefix-help-command)
   (add-to-list 'display-buffer-alist '("\\`\\*Embark Collect \\(Live\\|Completion\\)\\*"
-				       nil
-				       (window-parameters (mode-line-format . none)))))
+                                     nil
+                                     (window-parameters (mode-line-format . none)))))
 
 (use-package embark-consult
   :hook
@@ -161,12 +169,12 @@
 
 (use-package helpful
   :bind (("C-h x" . helpful-callable)
-	 ("C-h v" . helpful-variable)
-	 ("C-h f" . helpful-command)
-	 ("C-h F" . helpful-function)
-	 ("C-h k" . helpful-key)
-	 ("C-h M" . helpful-macro)
-	 ("C-h ." . helpful-at-point)))
+         ("C-h v" . helpful-variable)
+         ("C-h f" . helpful-command)
+         ("C-h F" . helpful-function)
+         ("C-h k" . helpful-key)
+         ("C-h M" . helpful-macro)
+         ("C-h ." . helpful-at-point)))
 
 (use-package auctex
   :custom
